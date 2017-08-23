3bc4b65d72a12f4f4d3764c7734349406d18d2e9
naveen <naveenmaturu@gmail.com>
2017-08-23 14:35:56 +0530

    Task Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  a;
+  b;
+  c;
+  next(){
+	  this.a = false;
+	  this.b= false;
+	  this.c=true;
+	  
+  }
+  next1(){
+	  alert("next1");
+	  this.a = true;
+	  this.b = false;
+	  this.c=false;
+  }
+  next2(){
+	  alert("next3");
+	  this.a = false;
+	  this.b = true;
+	  this.c=false;
+  }
+  
 }
